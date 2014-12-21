9f11920209ff7d8af20e5522fe3204d639008a2a
kai zhu <kaizhu256@gmail.com>
2014-12-21 15:06:57 +0800

    create node-install.sh

---
+++ README.md
@@ -0,0 +1,36 @@
+node-install
+============
+shell script to install nodejs on vanilla linux vm
+
+
+
+## build status [![travis.ci-org build status](https://api.travis-ci.org/kaizhu256/node-install.svg)](https://travis-ci.org/kaizhu256/node-install)
+
+
+
+## usage example
+```
+# download and install nodejs on linux vm
+eval "$(curl -fLSs https://raw.githubusercontent.com/kaizhu256/node-install/beta/node-install.sh)"
+```
+
+
+
+## package content
+- .travis.yml
+  - travis-ci config file
+- README.md
+  - readme file
+- node-install.sh
+  - shell script installing nodejs
+
+
+
+## todo
+- none
+
+
+
+## changelog
+#### 2014.12.21
+- initial commit
---
+++ node-install.sh
@@ -0,0 +1,32 @@
+shNodeInstall() {
+  # this function installs nodejs
+  # if nodejs is already installed, then return
+  if [ $(node -e "console.log(process.version)") = "v0.10.34" ]
+  then
+    return
+  fi
+  # init $NODE_PREFIX
+  local NODE_PREFIX=node-v0.10.34-linux-x64;
+  # create dir $HOME/bin
+  mkdir -p $HOME/bin || return $?
+  # download and untar nodejs
+  curl -#L http://nodejs.org/dist/v0.10.34/$NODE_PREFIX.tar.gz |\
+    tar -C $HOME/bin -xz || return $?
+  # install nodejs
+  rm -f $HOME/bin/node && ln -s $NODE_PREFIX/bin/node $HOME/bin/node || return $?
+  # install npm binary
+  rm -f $HOME/bin/npm && ln -s $NODE_PREFIX/bin/npm $HOME/bin/npm || return $?
+  # add $HOME/bin to $PATH
+  export PATH=$HOME/bin:$PATH
+  # test nodejs
+  printf 'testing nodejs ...\n'
+  if [ $(node -e "console.log(process.version)") = "v0.10.34" ]
+  then
+    printf 'test passed\n'
+  else
+    printf 'test failed\n'
+    return 1
+  fi
+}
+
+shNodeInstall
